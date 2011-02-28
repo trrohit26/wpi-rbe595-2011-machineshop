089457b2f72d3004f533a3e883664fab70ed2a47
aaronf0 <aaronf0@9defaee3-bef9-a6d8-8018-f130459a8257>
2011-02-28 01:57:27 +0000

    face -> hand

--- trunk/face.py
+++ trunk/face.py
@@ -1,14 +1,36 @@
 #!/usr/bin/python
 
-# Face Detection using OpenCV. Based on sample code by Roman Stanchak
-# Nirav Patel http://eclecti.cc 5/20/2008
+# Copyright (c) 2011, Aaron Fineman
+# Based off code by Roman Stanchak and Nirav Patel (http://eclecti.cc)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the <organization> nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import sys, os
 from opencv.cv import *
 from opencv.highgui import *
 
 haar_file = 'haar/1256617233-1-haarcascade_hand.xml'
-image_file = 'pictures/hand4.jpg'
 
 def detectObject(image):
   grayscale = cvCreateImage(cvSize(640, 480), 8, 1)
@@ -17,28 +39,28 @@ def detectObject(image):
   cvClearMemStorage(storage)
   cvEqualizeHist(grayscale, grayscale)
   cascade = cvLoadHaarClassifierCascade(haar_file, cvSize(1,1))
-  faces = cvHaarDetectObjects(grayscale, cascade, storage, 1.2, 2, 
+  objects = cvHaarDetectObjects(grayscale, cascade, storage, 1.2, 2, 
                               CV_HAAR_DO_CANNY_PRUNING, cvSize(100,100))
-  print faces
-  if faces:
-    for i in faces:
+  print objects
+  if objects:
+    for i in objects:
       cvRectangle(image, cvPoint( int(i.x), int(i.y)),
                   cvPoint(int(i.x+i.width), int(i.y+i.height)),
                   CV_RGB(0,255,0), 3, 8, 0)
   
 def displayObject(image):
-  cvNamedWindow("face", 1)
-  cvShowImage("face", image)
+  cvNamedWindow("objects", 1)
+  cvShowImage("objects", image)
   cvWaitKey(0)
-  cvDestroyWindow("face")
+  cvDestroyWindow("objects")
   
 def main():
   # Uses xawtv. Gstreamer can be used instead, but I found it much slower
-  os.system("v4lctl snap jpeg 640x480 /tmp/face.jpg")
-  image = cvLoadImage("/tmp/face.jpg")
+  os.system("v4lctl snap jpeg 640x480 /tmp/cam.jpg")
+  image = cvLoadImage("/tmp/cam.jpg")
   detectObject(image)
   displayObject(image)
-  cvSaveImage("/tmp/face.jpg", image)
+  cvSaveImage("/tmp/cam.jpg", image)
 
 if __name__ == "__main__":
   main()
